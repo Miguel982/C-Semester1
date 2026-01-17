fa9c0e6e3b71cf11001362e6e9635e8f3629cd4f
Miguel982 <mike.lulic@gmail.com>
2026-01-17 12:35:53 -0500

    question 6

---
+++ Assignment1/PackageCalculator.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    double weight, length, width, height;
+    double dimensionalWeight, finalWeight, cost;
+
+    cout << "Enter the package weight in kilograms: ";
+    cin >> weight;
+
+    cout << "Enter the package dimensions (length, width, height) in centimeters: ";
+    cin >> length;
+    cin >> width;
+    cin >> height;
+
+    dimensionalWeight = (length * width * height);
+    dimensionalWeight /= 5000;
+
+    //Forces 2 decimal places
+    cout << fixed << setprecision(2);
+
+    cout << "The dimensional weight is " << dimensionalWeight << " kg." << endl;
+
+    if (dimensionalWeight > weight) {
+        finalWeight = dimensionalWeight;
+    }
+    else {
+        finalWeight = weight;
+    }
+
+    if (finalWeight <= 2){
+        cost = 5;
+    }
+    else if (finalWeight > 2 && finalWeight <= 10) {
+        cost = 10;
+    }
+    else if (finalWeight > 10 && finalWeight <= 20) {
+        cost = 20;
+    }
+    else {
+        cost = -1;
+    }
+
+    if (cost > 0){
+        cout << "The shipping cost is $" << cost;
+    }
+    else {
+        cout << "The package is too heavy to be shipped.";
+    }
+
+    return 0;
+}
