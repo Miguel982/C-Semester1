c6569db819da8089ecdea204ecbdf9fb349b3492
Miguel982 <mike.lulic@gmail.com>
2026-02-02 21:21:19 -0500

    Finished Q1 Assgnment 2

--- Assignments/Assignment 2/Question1.cpp
+++ Assignments/Assignment 2/Question1.cpp
@@ -3,16 +3,20 @@
 using namespace std;
 
 int main(){
+
+    void printRoof(int); //Declare method to print the top of the house
+    void printBase(int, int); //Declare method to print the bottom of the house
+
     string userName; //Contains the user name
     string userInput; //The user's answer to the yes or no question
 
-    int houseCounter; //Contains the number of houses the user has created
-    int errorCounter; //Used to see how many times the user has failed to enter an even number
+    int houseCounter = 0; //Contains the number of houses the user has created
+    int errorCounter = 0; //Used to see how many times the user has failed to enter an even number
     int heightHouse; //Holds the width of the house
     int widthHouse; //Holds the height of the house
 
 
-    cout << "What is your name?";
+    cout << "What is your name? ";
     cin >> userName;
 
     cout << "Well " << userName << ", welcome to my house drawing program.";
@@ -22,7 +26,7 @@ int main(){
         //Would you like a house
         while (true){
 
-            cout << "Do you want me to draw a simple house for you? (yes/no)";
+            cout << "Do you want me to draw a simple house for you? (yes/no) ";
             cin >> userInput;
 
             if(userInput.compare("yes") == 0){
@@ -30,7 +34,7 @@ int main(){
                 break; //Exits while true loop
             }
 
-            else if (userInput.compare("no")){
+            else if (userInput.compare("no") == 0){
                 
                 string house = houseCounter > 1 ? " houses!" : " house!";
 
@@ -46,25 +50,25 @@ int main(){
 
             else {
 
-                cout << "Please enter yes or no"; //Then asks the question again
+                cout << "Please enter yes or no" << endl; //Then asks the question again
             }
         }
 
         //Enter the height of the house
-        cout << "Enter height of the house you want me to draw:";
+        cout << "Enter height of the house you want me to draw: ";
         cin >> heightHouse;
 
         //Enter the width of the house
         while(true)
         {
-            cout << "Please enter an even number for the width of the house (must be even numbers and bigger than 2):";
+            cout << "Please enter an even number for the width of the house (must be even numbers and bigger than 2): ";
             cin >> widthHouse;
 
-            if (widthHouse % 2 != 0){
+            if (widthHouse % 2 != 0 && widthHouse <= 2){
 
                 if(errorCounter <= 3){
 
-                    cout << "You enter " << widthHouse << " for the width. Not an even number!";
+                    cout << "You enter " << widthHouse << " for the width. Not an even number!" << endl;
                     errorCounter++;
                     continue; //Lets the user re-enter their value
                 }
@@ -78,7 +82,62 @@ int main(){
 
             break; //Exits while(true) loop
         }
+
+        printRoof(widthHouse);
+        printBase(widthHouse, heightHouse);
+        houseCounter++;
+        
     }
 
     return 0;
+}
+
+
+void printRoof(int width){
+
+    int spaces = (width / 2) - 1;
+
+    string output = "";
+
+    for(int i = 1; i <= spaces; i++){
+        output.append(" ");
+    }
+
+    output.append("*");
+
+    for (int i = 1; i <= (width/2) ; i++ ){
+        output.append("*");
+        output[output.length() - (i * 2)] = '*';
+
+        cout << output << endl;
+    }
+
+    return;
+}
+
+void printBase(int width, int height){
+
+    string layer = "";
+
+    for(int i = 0; i <= height; i++){
+        layer = "";
+
+        if (i == height){
+            for (int j = 0; j < width; j++){
+                layer.append("-");
+            }
+            cout << layer << endl;
+            return;
+        }
+
+        layer.append("|");
+
+        for (int j = 0; j < width - 2; j++) {
+            layer.append(" ");
+        }
+
+        layer.append("|");
+
+        cout << layer << endl;
+    }
 }
--- Tutorials/Tutorial 2/Question4.cpp
+++ Tutorials/Tutorial 2/Question4.cpp
@@ -4,12 +4,14 @@ using namespace std;
 
 int main(){
     void pyramid(int);
+    void pyramid2(int);
     void pattern1(int);
     void pattern2(int);
     void pattern3(int);
 
-    
-    pyramid(5);
+    int num = 8;
+
+    pyramid2(num);
 
     return 0;
 }
@@ -25,15 +27,15 @@ void pyramid(int size){
     for(int i = 1; i <= spaces; i++){
         output.append(" ");
     }
-
+    
     for (int i = 1; i <= size; i++){
         output.append("*");
-
-        cout << output << endl;
         
         if (output.length() != lengthLastRow) {
             output[output.length() - (i * 2)] = '*';
         }
+
+        cout << output << endl;
     }
 
     return;
@@ -49,4 +51,26 @@ void pattern2(int size){
 
 void pattern3(int size){
     
+}
+
+void pyramid2(int width){
+
+    int spaces = (width / 2) - 1;
+
+    string output = "";
+
+    for(int i = 1; i <= spaces; i++){
+        output.append(" ");
+    }
+
+    output.append("*");
+
+    for (int i = 1; i <= (width/2) ; i++ ){
+        output.append("*");
+        output[output.length() - (i * 2)] = '*';
+
+        cout << output << endl;
+    }
+
+    return;
 }
