65bd85028a88505b020ec899f227624e1c2d3785
Miguel982 <mike.lulic@gmail.com>
2026-03-05 18:38:35 -0500

    Assignment 3 Q2

--- Assignments/Assignment 3/Question2.cpp
+++ Assignments/Assignment 3/Question2.cpp
@@ -3,7 +3,30 @@
 
 using namespace std;
 
+bool testPalindrome(string, int, int);
+
 int main(){
+    string userInput;
+
+    cout << "Enter a word/sentence:" << endl;
+
+    //Gets the full sentence and not only the first word
+    getline(cin, userInput);
+
+    string result = testPalindrome(userInput, 0 , userInput.size() - 1)? "is" : "is not";
+    cout << userInput << " " << result << " a palindrome.";
 
     return 0;
+}
+
+bool testPalindrome(string sentence, int start, int end){
+    if(start >= end){
+        return true;
+    }
+    else if(sentence[start] == sentence[end]){
+        return testPalindrome(sentence, start + 1, end - 1);
+    }
+    else{
+        return false;
+    }
 }
